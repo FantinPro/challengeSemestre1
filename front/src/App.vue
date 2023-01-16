7919ee5ea0b539fdf0cd84bd0c425c937399c444
Naderi Farid <geekwid@gmail.com>
2023-01-16 18:01:02 +0100

    feat(front): add tailwindcss

--- front/src/App.vue
+++ front/src/App.vue
@@ -10,8 +10,8 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
+  </div>
 </template>
 
 <style scoped>
