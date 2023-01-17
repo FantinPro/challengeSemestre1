df64583bbf8149c9d9f88e5dce76015ee87ee6fa
Naderi Farid <geekwid@gmail.com>
2023-01-17 14:45:48 +0100

    fix: router

--- front/src/router.js
+++ front/src/router.js
@@ -1,22 +1,11 @@
-// 1. Define route components.
-// These can be imported from other files
-const Home = { template: '<div>Home</div>' }
-const Login = { template: '<div>Login</div>' }
+import * as VueRouter from 'vue-router'
+import Timeline from './views/Timeline.vue'
 
-
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
+  { path: '/', component: Timeline },
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
-const router = VueRouter.createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+export const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes,
 })
