b896a1e02747fb2efdc4123b1f6a78373a09e740
Naderi Farid <geekwid@gmail.com>
2023-01-18 12:09:31 +0100

    feat: sideMenu components

---
+++ front/src/components/Button/MenuButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <router-link v-bind="buttonProps" class="group">
+    <div
+      class="
+        flex
+        text-left text-white text-lg
+        font-medium
+        rounded-3xl
+        px-4
+        py-2
+        w-fit
+        group-hover:bg-opacity-10 group-hover:bg-white
+        transition
+        duration-200
+        ease-in-out
+        gap-2
+      "
+    >
+      <slot />
+    </div>
+  </router-link>
+</template>
+<script setup>
+const buttonProps = defineProps({
+  type: {
+    type: String,
+    default: "button",
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  to: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+<style>
+</style>
---
+++ front/src/components/Logo/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <img class="w-6 h-6" :src="Logo" alt="logo" />
+  </div>
+</template>
+<script setup>
+import Logo from "../../assets/logo.svg";
+</script>
