6684761bd23c408206c165942b925ccc710c6052
horek <azenoxe@gmail.com>
2023-01-18 12:51:25 +0100

    feat: on login set user in localStorage

--- front/src/router.js
+++ front/src/router.js
@@ -17,11 +17,10 @@ export const router = VueRouter.createRouter({
 
 function isAuthenticated() {
   const userToken = $cookies.get('echo_user_token')
-  console.log(!!userToken)
   return !!userToken
 }
 
-router.beforeEach(async (to, from, next) => {
+router.beforeEach(async (to, _, next) => {
  if ((to.path === '/login' || to.path === '/register') && isAuthenticated()) {
     next('/home')
  } else if (!(to.path === '/login' || to.path === '/register') && !isAuthenticated()) {
--- front/src/store/user.js
+++ front/src/store/user.js
@@ -2,12 +2,9 @@
 
 import { defineStore } from "pinia";
 
-
-
 export const useUserStore = defineStore('user', {
   state: () => ({
-    user: null,
-    userToken: $cookies.get('userToken'),
+    user: JSON.parse(localStorage.getItem('echoUser')),
   }),
   actions: {
     async signUp(values) {
@@ -20,15 +17,26 @@ export const useUserStore = defineStore('user', {
       });
     },
     async signIn(values) {
-      const res = await fetch('http://localhost:8000/api/login', {
+      const response = await fetch('http://localhost:8000/api/login', {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(values),
       });
-      const userToken = await res.json();
-      return userToken.token;
+      const userToken = await response.json();
+
+      $cookies.set(userToken.token, 'echo_user_token');
+
+      const res = await fetch('http://localhost:8000/api/users/me', {
+        headers: {
+          Authorization: `Bearer ${userToken.token}`,
+        },
+      });
+      const user = await res.json();
+      this.user = user;
+
+      localStorage.setItem('echoUser', JSON.stringify(user));
     },
   },
 });
