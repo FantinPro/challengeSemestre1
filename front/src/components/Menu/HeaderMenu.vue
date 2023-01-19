66959aab82c767647c668bc66a51ed25fa39103b
Naderi Farid <geekwid@gmail.com>
2023-01-19 16:36:20 +0100

    feat: add headerMenu

---
+++ front/src/components/Menu/HeaderMenu.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mt-5 border-b border-[#4c5157]">
+    <div class="px-6">
+      <slot name="title" />
+    </div>
+    <div class="mt-3">
+    <slot name="body" />
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
--- front/src/views/Timeline.vue
+++ front/src/views/Timeline.vue
@@ -1,28 +1,54 @@
 <template>
   <div class="flex flex-col overflow-auto h-full">
-    <span v-if="isLoading">Loading...</span>
-    <span v-else-if="isError">Error: {{ error.message }}</span>
-    <div v-if="data">
-      <div
-        v-for="feed in data"
-        :key="feed.id"
-      >
+    <HeaderMenu>
+      <template #title>
+        <h1 class="text-2xl font-bold cursor-pointer">Home</h1>
+      </template>
+      <template #body>
+        <div class="flex">
+          <button
+            class="
+              hover:bg-[#2f3336]
+              text-white
+              font-semibold
+              w-full
+              py-4
+            "
+          >
+            For you
+          </button>
+          <button
+            class="
+              hover:bg-[#2f3336]
+              text-white
+              font-semibold
+              w-full
+            "
+          >
+            Following
+          </button>
+        </div>
+      </template>
+    </HeaderMenu>
+    <div class="flex flex-col gap-2 p-4 mt-2">
+      <span v-if="isLoading">Loading...</span>
+      <span v-else-if="isError">Error: {{ error.message }}</span>
+      <div v-for="feed in data" :key="feed.id">
         <Card :item="feed" />
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import { reactive, onMounted } from "vue";
+import { onMounted } from "vue";
 import { useQuery } from "vue-query";
 import SideMenu from "../components/Menu/SideMenu.vue";
 import Card from "../components/Card/Card.vue";
+import HeaderMenu from "../components/Menu/HeaderMenu.vue";
 import LayoutDefault from "../layouts/LayoutDefault.vue";
 
-import { useUserStore } from "../store/user";
 import { useFeedStore } from "../store/feed";
 
-const { user } = useUserStore();
 const { fetchFeed } = useFeedStore();
 const emit = defineEmits(["update:layout"]);
 
@@ -33,5 +59,4 @@ onMounted(() => {
 const { isLoading, isError, data, error } = useQuery("feed", () =>
   fetchFeed(1)
 );
-
 </script>
