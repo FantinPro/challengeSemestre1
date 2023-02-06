352686f04f015c73e18d926f5166080c6a9ea1bd
Naderi Farid <geekwid@gmail.com>
2023-02-07 00:26:12 +0100

    fix: login and register forms

--- front/src/views/Login.vue
+++ front/src/views/Login.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="flex justify-center items-center h-full">
     <div class="flex flex-col rounded-2xl shadow-2xl bg-neutral-800/75 py-10 px-16 w-[500px]">
-      <h2 class="text-3xl font-bold mb-6 text-center">Connexion</h2>
-      <FormKit type="form" submit-label="Se connecter" :submit-attrs="{ outerClass: 'pt-4', inputClass: '!w-full !bg-primary-500' }" @submit="submit" >
-        <FormKit type="email" name="email" label="Email" validation="required" />
-        <FormKit type="password" name="password" label="Mot de Passe" validation="required" />
+      <div class="flex justify-between items-center">
+        <h1 class="text-3xl font-bold mb-6">Login</h1>
+        <div class="flex mb-6 items-center justify-center gap-3">
+          <h2 class="text-2xl font-medium">Echo</h2>
+          <img src="logo.svg" alt="Logo" class="w-10 h-10 invert" />
+        </div>
+      </div>
+      <FormKit type="form" submit-label="Login" :submit-attrs="{ outerClass: 'pt-4', inputClass: '!w-full !bg-primary-500 !font-medium' }" @submit="submit" >
+        <FormKit type="email" name="email" label="E-mail" validation="required" :classes="{ input: '!text-white' }" />
+        <FormKit type="password" name="password" label="Password" validation="required" :classes="{ input: '!text-white' }" />
       </FormKit>
       <span class="mt-3">
-        Pas encore de compte ?
-        <router-link to="/register" class="text-primary-400">Créer son compte</router-link>
+        New to Echo?
+        <router-link to="/register" class="text-primary-400">Create an account</router-link>
       </span>
     </div>
   </div>
