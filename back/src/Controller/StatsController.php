004c5634f60b19789f925d5f92023b37a6c148c8
Naderi Farid <geekwid@gmail.com>
2023-02-08 12:30:03 +0100

    feat: add stats and kpis

---
+++ back/src/Controller/StatsController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AdRepository;
+use App\Repository\MessageRepository;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[AsController]
+class StatsController extends AbstractController
+{
+    public function __construct(
+        private UserRepository $userRepository,
+        private AdRepository $adRepository,
+        private MessageRepository $messageRepository,
+    )
+    {}
+
+    #[Route('/api/stats', name: 'app_stats', methods: ['GET'])]
+    public function getStats(Request $request) {
+        $startDate = $request->query->get('startDate');
+        $endDate = $request->query->get('endDate');
+
+        // number registered users
+        $nbUsers = $this->userRepository->count([
+            'created' => [
+                'gte' => $startDate,
+                'lte' => $endDate
+            ]
+        ]);
+
+        // number ads
+        $nbAds = $this->adRepository->count([
+            'created' => [
+                'gte' => $startDate,
+                'lte' => $endDate
+            ]
+        ]);
+
+        // number messages
+        $nbMessages = $this->messageRepository->count([
+            'created' => [
+                'gte' => $startDate,
+                'lte' => $endDate
+            ]
+        ]);
+
+        // get Amount of money earned by ads
+        $amountEarned = $this->adRepository->getAmountEarned($startDate, $endDate);
+
+        return new JsonResponse([
+            'nbUsers' => $nbUsers,
+            'nbAds' => $nbAds,
+            'nbMessages' => $nbMessages,
+            'amountEarned' => $amountEarned
+        ]);
+    }
+}
